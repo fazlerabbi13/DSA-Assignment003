e62e91e2ef15addbf8cedc5794c93f49a21cd63f
fazlerabbi13 <mohammad0132794@gmail.com>
2025-07-29 00:22:07 +0600

    pallindrome array.

---
+++ palindromeArray.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;
+    vector<int> A(N);
+    
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i];
+    }
+    
+    bool isPalindrome = true;
+    for (int i = 0; i < N / 2; ++i) {
+        if (A[i] != A[N - 1 - i]) {
+            isPalindrome = false;
+            break;
+        }
+    }
+    
+    cout << (isPalindrome ? "YES" : "NO") << endl;
+    
+    return 0;
+}
